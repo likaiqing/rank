340a9cb6c3ac3d2756a3e8ef9fa1ec58d6a99418
likaiqing <likaiqing@panda.tv>
2019-02-20 16:54:55 +0800

    del CrossOrigin

--- src/main/java/com/pandatv/controller/RankController.java
+++ src/main/java/com/pandatv/controller/RankController.java
@@ -15,7 +15,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -30,7 +33,7 @@ import java.util.stream.Collectors;
  **/
 @Controller(value = "/rank")
 @RequestMapping("/rank")
-@CrossOrigin(origins = {"*"}, allowCredentials = "true")
+//@CrossOrigin(origins = {"*"}, allowCredentials = "true")
 public class RankController extends BaseController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
